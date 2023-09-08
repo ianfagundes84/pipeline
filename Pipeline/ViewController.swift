c8fa36975b431e867fa126424d3c214702bf8c2e
ianfagundes <ianfag@icloud.com>
2023-09-08 09:52:19 -0300

    Initial Commit

---
+++ Pipeline/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pipeline
+//
+//  Created by Ian Fagundes on 08/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
